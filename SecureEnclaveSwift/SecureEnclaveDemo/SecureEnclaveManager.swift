90bbd2971d8232bd43c8ba5fb2506ff8bd255544
JP Smith <jp.smith@ilstu.edu>
2016-11-17 14:45:15 -0600

    Replace files w/o submodule

---
+++ SecureEnclaveSwift/SecureEnclaveDemo/SecureEnclaveManager.swift
@@ -0,0 +1,55 @@
+//
+//  SecureEnclaveManager.swift
+//  BankAxept
+//
+//  Created by Håvard Fossli on 11.11.2016.
+//  Copyright © 2016 BankAxept. All rights reserved.
+//
+
+import Foundation
+
+final class SecureEnclaveManager {
+    
+    static let shared = SecureEnclaveManager()
+    
+    func deleteKeyPair() throws {
+        try helper.deletePublicKey()
+        try helper.deletePrivateKey()
+    }
+    
+    func publicKey() throws -> String {
+        let keys = try getKeys()
+        return keys.public.hex
+    }
+    
+    func verify(signature: Data, originalDigest: Data) throws -> Bool {
+        let keys = try getKeys()
+        return try helper.verify(signature: signature, digest: originalDigest, publicKey: keys.public.ref)
+    }
+    
+    func sign(_ digest: Data) throws -> Data {
+        let keys = try getKeys()
+        let signed = try helper.sign(digest, privateKey: keys.private)
+        return signed
+    }
+    
+    private let helper = SecureEnclaveHelper(publicLabel: "no.agens.demo.publicKey", privateLabel: "no.agens.demo.privateKey")
+    
+    private func getKeys() throws -> (`public`: SecureEnclaveKeyData, `private`: SecureEnclaveKeyReference) {
+        if let publicKeyRef = try? helper.getPublicKey(), let privateKey = try? helper.getPrivateKey() {
+            return (public: publicKeyRef, private: privateKey)
+        }
+        else {
+            var accessControlError: Unmanaged<CFError>?
+            let accessControl = SecAccessControlCreateWithFlags(kCFAllocatorDefault, kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly, [.userPresence, .privateKeyUsage], &accessControlError)
+            guard accessControl != nil else {
+                throw SecureEnclaveHelperError(message: "Could not generate access control. Error \(accessControlError?.takeRetainedValue())", osStatus: nil)
+            }
+            
+            let keypairResult = try helper.generateKeyPair(accessControl: accessControl!)
+            try helper.forceSavePublicKey(keypairResult.public)
+            
+            return (public: try helper.getPublicKey(), private: try helper.getPrivateKey())
+        }
+    }
+}
